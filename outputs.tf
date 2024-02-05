b573b3a3dcd1c66fe8c0fc15851756003adf4176
Muhammad Rudyn <nydurism@gmail.com>
2024-02-05 18:28:08 +0800

    Feb 5 commit

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 #Event 1
 output "invoke_url" {
   description = "API Gateway URL"
-  value = aws_api_gateway_stage.example.invoke_url
+  value       = aws_api_gateway_stage.example.invoke_url
 }
 
 #Event 2
 output "invoke_arn" {
   description = "Function ARN"
-  value = aws_lambda_function.hello_lambda.invoke_arn
+  value       = aws_lambda_function.hello_lambda.invoke_arn
 }
 
 #Event 3
 output "lambda_name" {
   description = "Function name"
-  value = aws_lambda_function.hello_lambda.function_name
+  value       = aws_lambda_function.hello_lambda.function_name
 }
